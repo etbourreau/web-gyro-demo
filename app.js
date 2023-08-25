a1094d3535fd4eec9d234f92c4676fb94e8aebb8
etbourreau <etbourreau@users.noreply.github.com>
2023-08-25 20:05:25 +0200

    tmp 10

--- app.js
+++ app.js
@@ -6,15 +6,12 @@ Vue.createApp({
     setup() {
         return {
             gyroData: ref({
-                alpha: 0,
                 beta: 0,
+                betaDelayed: 0,
                 gamma: 0,
+                gammaDelayed: 0,
             }),
             gyroRanges: {
-                alpha: {
-                    min: 0,
-                    max: 360,
-                },
                 beta: {
                     min: -180,
                     max: 180,
@@ -39,47 +36,84 @@ Vue.createApp({
                 screenAdjusted: false,
             })
             .then(() => {
-                this.msg = "GyroNorm initialized";
                 this.gyro.start((data) => {
-                    this.gyroData.alpha = round(data.do.alpha);
                     this.gyroData.beta = round(data.do.beta);
                     this.gyroData.gamma = round(data.do.gamma);
                 });
             })
             .catch((e) => {
                 const ways = {
-                    alpha: true,
                     beta: true,
                     gamma: true,
                 };
                 setInterval(() => {
-                    ["alpha", "beta", "gamma"].forEach((k) => {
-                        this.gyroData[k] += ways[k] ? 1 : -1;
+                    ["beta", "gamma"].forEach((k) => {
+                        const step = Math.floor(Math.random()  * 40 + 10)/10;
+                        this.gyroData[k] += ways[k] ? step : -step;
                         if (
                             this.gyroData[k] < this.gyroRanges[k].min ||
                             this.gyroData[k] > this.gyroRanges[k].max
                         ) {
-                            this.gyroData[k] += ways[k] ? -2 : 2;
+                            this.gyroData[k] += ways[k]
+                                ? -(step * 2)
+                                : step * 2;
                             ways[k] = !ways[k];
                         }
                         this.gyroData[k] = round(this.gyroData[k]);
                     });
                 }, 100);
             });
+        setInterval(() => {
+            const beta = this.gyroData.beta,
+                gamma = this.gyroData.gamma;
+            setTimeout(() => {
+                this.gyroData.betaDelayed = beta;
+                this.gyroData.gammaDelayed = gamma;
+            }, 1000);
+        }, 50);
 
         ondevicemotion = (e) => {
             this.aclData = e;
         };
     },
+    methods: {
+        getAvg: function (k) {
+            const current = this.gyroData[k];
+            const delayed = this.gyroData[k + "Delayed"];
+            const min = this.gyroRanges[k].min;
+            const max = this.gyroRanges[k].max;
+            const range = max - min;
+            const middle = (min + max) / 2;
+            const margin = range * 0.25;
+            const computedDelayed =
+                current < min + margin && delayed > middle
+                    ? delayed - range
+                    : current > max - margin && delayed < middle
+                    ? delayed + range
+                    : delayed;
+            let avg = (current + computedDelayed) / 2;
+            return avg + (avg < min ? range : avg > max ? -range : 0);
+        },
+    },
     template: `
       <div class="w-100 d-flex justify-content-center align-items-center flex-column">
         <div v-html="msg"></div>
         <div class="d-flex flex-column w-100">
-            <div class="w-100 d-flex flex-column align-items-center px-5" v-for="k in ['alpha', 'beta', 'gamma']">
-                <caption>{{k}}</caption>  
+            <div class="w-100 d-flex flex-column align-items-center px-5" v-for="k in ['beta', 'gamma']">
+                <caption>{{k}}</caption>
                 <div class="w-100 bar border">
                     <div class="h-100" :style="{
                         width: ((gyroData[k] - gyroRanges[k].min) / (gyroRanges[k].max - gyroRanges[k].min) * 100) + '%',
+                    }"></div>
+                </div>
+                <div class="w-100 bar border">
+                    <div class="h-100" :style="{
+                        width: ((getAvg(k) - gyroRanges[k].min) / (gyroRanges[k].max - gyroRanges[k].min) * 100) + '%',
+                    }"></div>
+                </div>
+                <div class="w-100 bar border">
+                    <div class="h-100" :style="{
+                        width: ((gyroData[k + 'Delayed'] - gyroRanges[k].min) / (gyroRanges[k].max - gyroRanges[k].min) * 100) + '%',
                     }"></div>
                 </div>
             </div>
